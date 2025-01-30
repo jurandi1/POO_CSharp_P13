eb9d5a9c5653ee96a63f8d25fc652dc249c714bb
Jurandi Junior <jurandijr20@gmail.com>
2025-01-29 21:19:01 -0300

    Bloco finally

--- Program.cs
+++ Program.cs
@@ -4,7 +4,31 @@
     {
         private static void Main(string[] args)
         {
+            //Bloco finally
+            Console.WriteLine();
+            Console.WriteLine("Bloco finally");
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
             // Estrutura try-catch 
+            Console.WriteLine();
+            Console.WriteLine("Com Estrutura try-catch");
             try
             {
                 int n1 = int.Parse(Console.ReadLine());
