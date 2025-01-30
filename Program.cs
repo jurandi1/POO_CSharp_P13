6895411ba343935636028b48eff137aa14fcfe19
Jurandi Junior <jurandijr20@gmail.com>
2025-01-29 21:10:45 -0300

    Estrutura try-catch

--- Program.cs
+++ Program.cs
@@ -4,6 +4,22 @@
     {
         private static void Main(string[] args)
         {
+            // Estrutura try-catch 
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero is not allowed");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error! " + e.Message);
+            }
         }
     }
 }
