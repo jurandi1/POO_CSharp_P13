6bc10ac2f06ebebd4d038f2341cfa9d1166e59de
Jurandi Junior <jurandijr20@gmail.com>
2025-02-01 12:18:57 -0300

    Criando exceções personalizadas P2

--- Entities/Reservation.cs
+++ Entities/Reservation.cs
@@ -21,10 +21,21 @@
             return (int)duration.TotalDays;
         }
 
-        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        public string UpdateDates(DateTime checkIn, DateTime checkOut)
         {
+
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                return "Reservation dates for update must be future dates.";
+            }
+            if (checkOut <= checkIn)
+            {
+                return "Check-out date must be after check-in date.";
+            }
             CheckIn = checkIn;
             CheckOut = checkOut;
+            return null;
         }
 
         public override string ToString()
